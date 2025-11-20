7dbcb74fe9b128e8a328b8b8daf6cf429eb18274
nezamutdinovdmitriy <nezamutdinovdmitriy@gmail.com>
2025-11-20 16:44:54 +0500

    20.11

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -1,14 +1,12 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class DragAndDrop : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
-    [SerializeField] private LayerMask _layerMask;
+    [SerializeField] private LayerMask _targetObjectMask;
+    [SerializeField] private LayerMask _groundMask;
 
     private Transform _target;
-    private Rigidbody _targetRigidbody;
-
 
     private void Update()
     {
@@ -17,34 +15,21 @@ public class DragAndDrop : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            if (Physics.Raycast(worldMouseRay, out RaycastHit hitInfo, Mathf.Infinity, _layerMask))
+            if (Physics.Raycast(worldMouseRay, out RaycastHit hitInfo, Mathf.Infinity, _targetObjectMask))
             {
                 _target = hitInfo.collider.gameObject.transform;
-
-                _targetRigidbody = _target.GetComponent<Rigidbody>();
-                _targetRigidbody.isKinematic = true;
 
                 Debug.Log(hitInfo.collider.name);
             }
         }
 
         if (Input.GetMouseButtonUp(0))
-        {
             _target = null;
-            _targetRigidbody.isKinematic = false;
-        }
 
         if (_target != null)
         {
-            float distanceToTarget = Vector3.Distance(_camera.transform.position, _target.transform.position);
-            screenMousePosition.z = distanceToTarget;
-
-            Vector3 worldMousePosition = _camera.ScreenToWorldPoint(screenMousePosition);
-
-            _target.position = new Vector3(worldMousePosition.x, _target.position.y, worldMousePosition.z);
-            
-            if (_target.position.y < 0)
-                _target.position = new Vector3(_target.position.x, 0, _target.position.z);
+            if(Physics.Raycast(worldMouseRay, out RaycastHit hitInfo, Mathf.Infinity, _groundMask))
+                _target.position = hitInfo.point;
         }
     }
 }
